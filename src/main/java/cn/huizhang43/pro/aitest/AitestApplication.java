f7104ae3458fa70b80ec7a40ccd2c210197e8db8
huizhang43 <huizhang43@iflytek.com>
2025-04-16 16:56:55 +0800

    1. 修复mcp client的sse版本

--- src/main/java/cn/huizhang43/pro/aitest/AitestApplication.java
+++ src/main/java/cn/huizhang43/pro/aitest/AitestApplication.java
@@ -1,5 +1,6 @@
 package cn.huizhang43.pro.aitest;
 
+import org.springframework.ai.autoconfigure.mcp.client.SseHttpClientTransportAutoConfiguration;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.boot.CommandLineRunner;
@@ -8,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SseHttpClientTransportAutoConfiguration.class})
 public class AitestApplication {
 
     public static void main(String[] args) {
@@ -16,25 +17,20 @@ public class AitestApplication {
     }
 
     @Bean
-    public CommandLineRunner predefinedQuestions(
-            ChatClient.Builder chatClientBuilder,
-            ToolCallbackProvider tools,
-            ConfigurableApplicationContext context) {
+    public CommandLineRunner predefinedQuestions(ChatClient.Builder chatClientBuilder,
+                                                 ToolCallbackProvider tools,
+                                                 ConfigurableApplicationContext context) {
         return args -> {
             // 构建ChatClient并注入MCP工具
             var chatClient = chatClientBuilder
                     .defaultTools(tools)
                     .build();
 
-            // 定义用户输入
+            // 使用ChatClient与LLM交互
             String userInput = "北京的天气如何？";
-            // 打印问题
             System.out.println("\n>>> QUESTION: " + userInput);
-            // 调用LLM并打印响应
-            System.out.println("\n>>> ASSISTANT: " +
-                    chatClient.prompt(userInput).call().content());
+            System.out.println("\n>>> ASSISTANT: " + chatClient.prompt(userInput).call().content());
 
-            // 关闭应用上下文
             context.close();
         };
     }
